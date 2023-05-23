31650936f9f3214c83a6e3a564dec59672d4518a
Carlos Solorzano <carlospoor91@gmail.com>
2023-05-23 14:10:35 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
     license='MIT',
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
-    packages=['keras_ed'],
+    packages=['kerased'],
     install_requires=install_requires
 )
